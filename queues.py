d95dd7644d9641300e900987cc3cd38c544cee3b
QuantPy <pythonforquants@gmail.com>
2023-10-21 17:42:03 +0200

    added dataclasses to store tweets and allow for storage ability in text files

---
+++ queues.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
